5c7aebcf78774fe33f8c09ca05b1963a3a7a10f8
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-04-14 16:39:15 +0900

    Code linting (manually)

--- dev.js
+++ dev.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const execa = require('execa')
+const execa = require('execa');
 
-const args = require('arg')({})
+const args = require('arg')({});
 
-execa.sync('node', ['index.js', 'workspace', '--example', args._[0] || 'with-javascript'], { stdio: 'inherit' })
+execa.sync('node', ['index.js', 'workspace', '--example', args._[0] || 'with-javascript'], { stdio: 'inherit' });
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
-const { resolve } = require('path')
-const arg = require('arg')
-const chalk = require('chalk')
+const { resolve } = require('path');
+const arg = require('arg');
+const chalk = require('chalk');
 
 const args = arg({
   '--help': Boolean,
   '--version': Boolean,
   '--example': String,
   '-h': '--help',
-  '-v': '--version'
-})
+  '-v': '--version',
+});
 
 if (args['--version']) {
-  const pkg = require(resolve(__dirname, './package.json'))
-  console.log(`create-nextron-app v${pkg.version}`)
-  process.exit(0)
+  const pkg = require(resolve(__dirname, './package.json'));
+  console.log(`create-nextron-app v${pkg.version}`);
+  process.exit(0);
 }
 
 if (args['--help'] || (!args._[0])) {
@@ -33,92 +33,92 @@ if (args['--help'] || (!args._[0])) {
       --help,     -h                      shows this help message
       --version,  -v                      displays the current version of create-nextron-app
       --example,  -e {underline example_folder_name}  sets the example as a template
-  `)
-  process.exit(0)
+  `);
+  process.exit(0);
 }
 
-createNextronApp()
+createNextronApp();
 
 async function createNextronApp() {
-  const spinner = require('./spinner')
-  const example = args['--example'] || 'with-javascript'
+  const spinner = require('./spinner');
+  const example = args['--example'] || 'with-javascript';
 
   try {
-    spinner.create('Validating existence...')
-    await validatesExistence(example)
+    spinner.create('Validating existence...');
+    await validatesExistence(example);
   } catch (error) {
-    spinner.fail(`Not found: ${example}`)
+    spinner.fail(`Not found: ${example}`);
   }
 
   try {
-    spinner.create('Downloading and extracting...')
-    const name = resolve(process.cwd(), args._[0])
-    await require('make-dir')(name)
-    await extract(name, example)
+    spinner.create('Downloading and extracting...');
+    const name = resolve(process.cwd(), args._[0]);
+    await require('make-dir')(name);
+    await extract(name, example);
 
-    const cmd = (await pm() === 'yarn') ? 'yarn && yarn dev' : 'npm install && npm run dev'
-    spinner.clear(`Run \`${cmd}\` inside of "${name}" to start the app`)
+    const cmd = (await pm() === 'yarn') ? 'yarn && yarn dev' : 'npm install && npm run dev';
+    spinner.clear(`Run \`${cmd}\` inside of "${name}" to start the app`);
   } catch (error) {
-    spinner.fail(error)
+    spinner.fail(error);
   }
 }
 
 async function validatesExistence(example) {
-  const Github = require('@octokit/rest')
+  const Github = require('@octokit/rest');
   await new Github().repos.getContent({
     owner: 'saltyshiomix',
     repo: 'nextron',
-    path: `examples/${example}/package.json`
-  })
+    path: `examples/${example}/package.json`,
+  });
 }
 
 async function extract(name, example) {
-  const masterUrl = 'https://codeload.github.com/saltyshiomix/nextron/tar.gz/master'
-  const got = require('got')
-  const { t, x } = require('tar')
+  const masterUrl = 'https://codeload.github.com/saltyshiomix/nextron/tar.gz/master';
+  const got = require('got');
+  const { t, x } = require('tar');
 
-  let ext = 'js'
+  let ext = 'js';
   await got
     .stream(masterUrl)
     .pipe(t({ cwd: name, strip: 3, filter: (path) => {
       if (path.endsWith(`${example}/tsconfig.json`)) {
-        ext = 'ts'
+        ext = 'ts';
       }
-      return false
+      return false;
     }}))
     .on('finish', async () => {
       await got
         .stream(masterUrl)
-        .pipe(x({ cwd: name, strip: 4 }, [`nextron-master/examples/_template/${ext}`]))
+        .pipe(x({ cwd: name, strip: 4 }, [`nextron-master/examples/_template/${ext}`]));
 
       await got
         .stream(masterUrl)
-        .pipe(x({ cwd: name, strip: 3 }, [`nextron-master/examples/${example}`]))
-    })
+        .pipe(x({ cwd: name, strip: 3 }, [`nextron-master/examples/${example}`]));
+    });
 }
 
 async function pm() {
-  const { promisify } = require('util')
-  const { exec: defaultExec } = require('child_process')
+  const { promisify } = require('util');
+  const { exec: defaultExec } = require('child_process');
 
-  let pm = 'yarn'
-  const cwd = process.cwd()
-  const exec = promisify(defaultExec)
+  let pm = 'yarn';
+  const cwd = process.cwd();
+  const exec = promisify(defaultExec);
   try {
-    await exec(`${pm} -v`, { cwd })
+    await exec(`${pm} -v`, { cwd });
   } catch (_) {
-    pm = 'npm'
+    pm = 'npm';
     try {
-      await exec(`${pm} -v`, { cwd })
+      await exec(`${pm} -v`, { cwd });
     } catch (_) {
-      pm = null
+      pm = null;
     }
   }
 
   if (pm === null) {
-    console.log(chalk.red('No available package manager! (`yarn` or `npm` is needed)'))
-    process.exit(1)
+    console.log(chalk.red('No available package manager! (`yarn` or `npm` is needed)'));
+    process.exit(1);
   }
 
-  return pm
+  return pm;
 }
