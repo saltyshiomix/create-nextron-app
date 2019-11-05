519aee70dd48c87cb42522ac77e4fdad354e5b7d
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-05 19:50:08 +0900

    Refactoring (dev.js)

--- dev.js
+++ dev.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-const execa = require('execa');
-
 const args = require('arg')({});
 
-execa.sync('node', ['index.js', 'workspace', '--example', args._[0] || 'with-javascript'], { stdio: 'inherit' });
+require('execa').sync('node', ['index.js', 'workspace', '--example', args._[0] || 'with-javascript'], { stdio: 'inherit' });
